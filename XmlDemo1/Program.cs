bf438af586082d084a96a33818dcae24d33420b8
Patrikas Adomaitis <ado.patrikas@gmail.com>
2021-02-11 16:43:59 +0200

    Pridejom naujus elementus i xml dokumentus

--- XmlDemo1/Program.cs
+++ XmlDemo1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace XmlDemo1
 {
@@ -10,6 +11,15 @@ namespace XmlDemo1
     {
         static void Main(string[] args)
         {
+            XmlDocument asmuoDocument = new XmlDocument();
+            XmlElement asmuo = asmuoDocument.CreateElement("Asmuo");
+            asmuoDocument.AppendChild(asmuo);
+            var vardas = asmuoDocument.CreateElement("Vardas");
+            vardas.InnerText = "Jonas";
+            asmuo.AppendChild(vardas);
+
+            Console.WriteLine(asmuoDocument.OuterXml);
+            Console.ReadKey();
         }
     }
 }
